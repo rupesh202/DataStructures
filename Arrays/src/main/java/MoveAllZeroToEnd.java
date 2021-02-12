e7f100e165694056f40040ead88947f1081adfa0
rupeshmalkar <rupesh202@gmail.com>
2021-02-12 17:36:10 +0530

    MoveAllZeroToEnd

---
+++ Arrays/src/main/java/MoveAllZeroToEnd.java
@@ -0,0 +1,21 @@
+public class MoveAllZeroToEnd {
+    public static void main(String[] args) {
+        MoveAllZeroToEnd moveAllZeroToEnd = new MoveAllZeroToEnd();
+        int[] input = new int[]{5,8,0,10,20,0,0,6,0,9}; // {5,8,10,0,20,0,0,6,0,9} //
+    }
+
+    public int[] moveAllZero(int[] input){
+        int nonZeroCount =0;
+        for (int i=0; i<input.length; i++){
+
+            if (input[i] != 0){
+
+                int temp = input[i];
+                input[i] = input[nonZeroCount];
+                input[nonZeroCount] = temp;
+                nonZeroCount++;
+            }
+        }
+        return input;
+    }
+}
