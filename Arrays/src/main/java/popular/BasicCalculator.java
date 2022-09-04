271a0ee6fd72b2f5704506b30a1afdb511263b5c
rupeshmalkar <rupesh202@gmail.com>
2022-09-04 14:26:37 +0530

    calculator leetcode 227

---
+++ Arrays/src/main/java/popular/BasicCalculator.java
@@ -0,0 +1,61 @@
+package popular;
+
+import java.util.Stack;
+
+/*
+Given a string s which represents an expression, evaluate this expression and return its value.
+
+The integer division should truncate toward zero.
+
+You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
+
+Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+
+https://leetcode.com/explore/interview/card/top-interview-questions-hard/116/array-and-strings/836/
+
+Solution:
+https://www.youtube.com/watch?v=2EErQ25kKE8
+https://leetcode.com/problems/basic-calculator-ii/solution/
+
+ */
+public class BasicCalculator {
+    public static void main(String[] args) {
+        String input = "37-2*4";
+        BasicCalculator obj = new BasicCalculator();
+        System.out.println(obj.calculator(input));
+    }
+    public int calculator(String s){
+        if (s == null || s.isEmpty()) return 0;
+        int len = s.length();
+        Stack<Integer> stack = new Stack<Integer>();
+        int currentNumber = 0;
+        char operation = '+';
+        for (int i = 0; i < len; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == len - 1) {
+                if (operation == '-') {
+                    stack.push(-currentNumber);
+                }
+                else if (operation == '+') {
+                    stack.push(currentNumber);
+                }
+                else if (operation == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                }
+                else if (operation == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
+    }
+}
